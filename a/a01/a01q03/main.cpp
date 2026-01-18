0b710b29a86f4abfabd9053ce198e442b8fb944e
nalahaideb <nasseralahaideb@hotmail.com>
2026-01-17 21:15:40 -0600

    work on a01

--- a/a01/a01q03/main.cpp
+++ a/a01/a01q03/main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<algorithm>
 #include<vector>
+
 std::ostream & operator<<(std::ostream & cout, const std::pair<int, int> & x)
 {
   std::cout << '<' << x.first << ", " << x.second << '>';         
@@ -16,16 +17,34 @@ int main()
 {
   for (int i = 0; i < 10; ++i)
   {
-    const int N = rand() % 10;
+    const int N = rand() % 10 + 5;
+    std::cout << "N = " << N << std::endl;
     std::vector< std::pair<int, int> > nums;
     std::vector< std::pair<int, int> > candidates;
     randgen(nums);
+    std::sort(nums.begin(), nums.end(), [](auto &left, auto &right) { return left.first < right.first; });
+
+    std::cout << "nums array = ["; 
+    for (auto i = nums.begin(); i != nums.end(); ++i)
+    {
+      std::cout << i->first << ", ";
+    }
+    std::cout << "]\n";
     std::pair<int, int> index = check_loop(nums, candidates, N);
+    std::cout << index << std::endl;
+    
+    std::cout << '[';
+    for (int k = 0; k < candidates.size(); ++k)
+    {
+      std::cout << candidates[k] << ", ";
+    }
+    std::cout << "]\n";
   }
 }
 
 void check_candidate(std::vector< std::pair<int, int> > & x, const std::pair<int, int> & c, const std::pair<int, int> & d, const int N)
 {
+  //std::cout << "c + d = " << c.first << '+' << d.first << std::endl;
   if (c.first + d.first == N)
   {
     x.push_back({c.second, d.second});
@@ -44,12 +63,12 @@ void randgen(std::vector< std::pair<int, int> > & x)
 //this list should be sorted
 std::pair<int, int> check_loop(const std::vector< std::pair<int, int> > & x, std::vector< std::pair<int, int> > & y, const int N)
 {
-  for (int i = 0; i < x.size() || x[i].first >= N; ++i)
+  for (int i = 1; i < x.size(); ++i)
   {
-    check_candidate(y, x[i], x[i + 1], N);
+    if (x[i].first > N)    
+      check_candidate(y, x[i], x[i - 1], N);
   }
-  
-  std::sort(y.begin(), y.end());
+  //std::sort(y.begin(), y.end());
   std::pair<int, int> deft = {-1, -1}; 
   return (y.empty() ? deft : y[0]);
 }
---
+++ a/a01/a01q05/main.cpp
@@ -0,0 +1,105 @@
+
+#include<iostream>
+#include<vector>
+
+bool is_valid(const std::vector<int> &x);
+
+void randgen(std::vector<int> & x);
+
+void print(const std::vector<int> & x);
+void print_line(const std::vector<int> & x, const int start);
+
+int main()
+{
+  std::vector<int> sudoku;
+  randgen(sudoku);
+  print(sudoku);
+  for (int i = 0; i < sudoku.size(); ++i)
+  {
+    if (i > 0 && i % 9 == 0)
+      std::cout << " | ";
+    std::cout << sudoku[i] << ' ';
+  }
+  std::cout << std::endl << "size = " << sudoku.size() << std::endl;
+  return 0;
+}
+
+bool is_valid(const std::vector<int> & x)
+{
+  for (int i = 0; i < x.size(); ++i)
+  {
+    
+  }
+  return true;
+}
+
+void randgen(std::vector<int> & x)
+{
+  for (int i = 0; i < 9; ++i)
+  {
+    //bool checklist of nums
+    bool nums[10] = {0};
+    
+    //set 0 to true
+    nums[0] = 1;
+    
+    int valid_bool_row = 0;
+    while (valid_bool_row < 9)
+    {
+      int f = rand() % 10;
+      if (!nums[f])
+      {
+        x.push_back(f);
+        nums[f] = true;
+        ++valid_bool_row;
+      }
+      else if (f == 0)
+      {
+        x.push_back(0);
+        ++valid_bool_row;
+      }
+      std::cout << "\n";
+    }
+  }
+}
+
+//prints a line of the first 3 triples of digits, 9 nums
+void print_line(const std::vector<int> & x, const int start)
+{
+  int offset = start;
+  for (int i = 0; i < 3; ++i)
+  {
+    for (int k = 0; k < 3; ++k)
+    {
+      if (x[offset + k] > 0)
+        std::cout << " " << x[offset + k];
+      else
+        std::cout << " .";
+    }
+    offset += 9;
+    std::cout << " |";
+  }
+  std::cout << std::endl;
+}
+
+void print(const std::vector<int> & x)
+{
+  const std::string bar = "+-------+-------+-------+";
+
+  std::cout << bar << "\n";
+  int offset = 0;
+  for (int i = 0; i < 9; ++i)
+  {
+    std::cout << "|";
+
+    print_line(x, i * 3);
+    //horizontal divider
+    if ((i + 1) % 3 == 0 && i < 8)
+    {
+      std::cout << bar << "\n";
+    }
+    offset += 3;
+  }
+
+    std::cout << bar << "\n"; // Print bottom border
+}
